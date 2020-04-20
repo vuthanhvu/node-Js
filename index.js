fc2ef0b9894289eed25d3e3134c33f9864f38d08
PhongVTP <vuphong213@gmail.com>
2020-04-21 06:44:36 +0700

    add inex.js

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ var express = require('express');
 var app = express(); //instant gọi 1 app instant -> 1 design pattern
 var port = 3000;
 
+app.set('views', './views')
+app.set('view engine', 'pug')
 app.get('/', function(request, response) {
-    response.send('<h1>hello Phong</h1>');
+    response.render('index', {
+        name: "QuynhQuynh"
+    });
 });
 
 app.get('/user', function(req, res) {
-    res.send('User');
+    res.render('user/index', {
+        users:[
+        { id: 1, name: 'Tu'},
+        { id: 2, name:'Anh'}
+        ]
+    });
 })
 
 app.listen(port, function () {
-    console.log('aaaaa port' + port);
+    console.log('port' + port);
 })
